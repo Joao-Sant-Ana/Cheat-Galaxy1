0ea498acb1c98a8b00c1d1e86ba37e6c8541a0ab
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-06-16 14:31:33 -0300

    Criação do js
    Próximo obejtivo:
    Animação de login/registro.

---
+++ login.js
@@ -0,0 +1,13 @@
+var $right = document.getElementById('right')
+var $left = document.getElementById('left')
+var $subRight = document.getElementsByClassName('sub')[0]
+var $subLeft = document.getElementsByClassName('sub')[1]
+var $login = document.getElementById('login')
+var $userLogin = document.getElementById('username-login')
+var $passLogin = document.getElementById('password-login')
+var $btnLogin = document.getElementById('login-btn')
+var $register = document.getElementById('register')
+var $userRegis = document.getElementById('username-regis')
+var $passRegis = document.getElementById('password-regis')
+var $btnRegis = document.getElementById('btn-regis')
+
