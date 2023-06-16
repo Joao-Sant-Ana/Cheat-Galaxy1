090d329909826b6395c38c6041f69ff810e45cd3
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-06-16 17:24:14 -0300

    Lado esquerdo concluido
    Lado esquerdo da animação feito, falta apenas fazer o lado direito.
    
    Acresentar tarefa:
    Deixar a tela de login com melhor responsividade.

--- login.js
+++ login.js
@@ -9,16 +9,43 @@ var $btnLogin = document.getElementById('login-btn')
 var $register = document.getElementById('register')
 var $userRegis = document.getElementById('username-regis')
 var $passRegis = document.getElementById('password-regis')
-var $btnRegis = document.getElementById('btn-regis')
+var $btnRegis = document.getElementById('register-btn')
 
 $registext.addEventListener('click', clique)
 
 function clique() {
-    $left.style.animation = 'changeColor'
-    $registext.style.border = 'none'
-    $registext.style.transition = '2s'
-    $registext.style.transform = 'translatey(0px)'
-    setTimeout(() => {
-        $registext.style.marginTop = '0'+'%'
-    }, 1)
+    $left.style.pointerEvents = 'all'
+    
+    //Animação da cor\\
+
+    $left.style.transitionDuration = '1.5s'
+    $left.style.backgroundColor = 'white'
+
+    //Animação do texto subindo\\
+
+    $registext.style.transition = '1.5s'
+    $registext.style.marginTop = '0'+'%'
+    $registext.style.border = '2px solid white'
+    $registext.style.color = 'black'
+    $registext.style.backgroundColor = 'white'
+    $registext.style.cursor = 'default'
+
+    //Animação dos inputs\\
+
+    $userRegis.style.transition = '1.5s'
+    $userRegis.style.backgroundColor = 'white'
+    $userRegis.style.border = '1px solid black'
+    $userRegis.style.pointerEvents = 'all'
+
+    $passRegis.style.transition = '1.5s'
+    $passRegis.style.backgroundColor = 'white'
+    $passRegis.style.border = '1px solid black'
+    $passRegis.style.pointerEvents = 'all'
+
+    $btnRegis.style.transition = '1.5s'
+    $btnRegis.style.backgroundColor = 'white'
+    $btnRegis.style.border = '1px solid black'
+    $btnRegis.style.backgroundColor = 'Grey'
+    $btnRegis.style.cursor = 'pointer'
+    $btnRegis.style.pointerEvents = 'all'
 }
